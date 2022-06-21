4fa78d86a1bad8a30afd9d9bbcbc1a0892882895
heechanshin <hs1916@gmail.com>
2022-06-21 10:17:55 +0900

    mongo URI config

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const {User} = require('./models/User');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://admin:7q8w9e0r!!@mymongo.gv5pbu2.mongodb.net/?retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
+
+const mongoose = require('mongoose');
+console.log(config.mongoURI)
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true
 }).then(() => console.log('MongoDB Connected....'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('안녕하세요! Web 입니다!')
+  res.send('안녕하세요! Web 입니다! 변경이요')
+})
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+
+
 })
 
 app.listen(port, () => {
